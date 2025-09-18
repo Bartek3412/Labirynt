84729f1d039052b42648ee12435f2c6dda9a72bd
Bartek3412 <bartek.babij.pro@gmail.com>
2025-09-18 20:00:10 +0200

    Player movment

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -2,15 +2,32 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    [SerializeField]float mouseSensitivity = 100f;
+    Transform playerBody;
+    float xRotation = 0f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        playerBody = transform.parent;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        CameraRotation();
+    }
+
+    void CameraRotation()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX); 
     }
 }
